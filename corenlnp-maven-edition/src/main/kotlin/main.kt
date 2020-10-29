d58a75daba25683a31b93b261a1b51f522375aca
Stephane Dubedat <steph.dubedat@gmail.com>
2020-10-29 15:37:12 +0000

    add initial POC

---
+++ corenlnp-maven-edition/src/main/kotlin/main.kt
@@ -0,0 +1,118 @@
+import edu.stanford.nlp.coref.data.CorefChain
+import edu.stanford.nlp.ie.util.*
+import edu.stanford.nlp.ling.*
+import edu.stanford.nlp.pipeline.*
+import edu.stanford.nlp.semgraph.*
+import edu.stanford.nlp.trees.*
+import java.util.*
+
+object BasicPipelineExample {
+    var text = "Joe Smith was born in California. " +
+            "In 2017, he went to Paris, France in the summer. " +
+            "His flight left at 3:00pm on July 10th, 2017. " +
+            "After eating some escargot for the first time, Joe said, \"That was delicious!\" " +
+            "He sent a postcard to his sister Jane Smith. " +
+            "After hearing about Joe's trip, Jane decided she might go to France one day."
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        // set up pipeline properties
+        val props = Properties()
+        // set the list of annotators to run
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma")//,ner")//,parse")//,depparse")//,coref,kbp,quote")
+        // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
+        props.setProperty("coref.algorithm", "neural")
+        // build pipeline
+        val pipeline = StanfordCoreNLP(props)
+        // create a document object
+        val document = CoreDocument(text)
+        // annnotate the document
+        pipeline.annotate(document)
+        // examples
+
+        // 10th token of the document
+        val token: CoreLabel = document.tokens().get(10)
+        println("Example: token")
+        System.out.println(token)
+        println()
+
+        // text of the first sentence
+        val sentenceText: String = document.sentences().get(0).text()
+        println("Example: sentence")
+        println(sentenceText)
+        println()
+
+        // second sentence
+        val sentence: CoreSentence = document.sentences().get(1)
+
+        // list of the part-of-speech tags for the second sentence
+        val posTags: List<String> = sentence.posTags()
+        println("Example: pos tags")
+        println(posTags)
+        println()
+
+        println(Runtime.getRuntime().freeMemory())
+
+        // list of the ner tags for the second sentence
+        /*val nerTags: List<String> = sentence.nerTags()
+        println("Example: ner tags")
+        println(nerTags)
+        println()*/
+
+        // constituency parse for the second sentence
+        /*val constituencyParse: Tree = sentence.constituencyParse()
+        println("Example: constituency parse")
+        System.out.println(constituencyParse)
+        println()*/
+
+        // dependency parse for the second sentence
+        /*val dependencyParse: SemanticGraph = sentence.dependencyParse()
+        println("Example: dependency parse")
+        System.out.println(dependencyParse)
+        println()*/
+
+        // kbp relations found in fifth sentence
+        /*val relations: List<RelationTriple> = document.sentences().get(4).relations()
+        println("Example: relation")
+        System.out.println(relations[0])
+        println()
+
+        // entity mentions in the second sentence
+        val entityMentions: List<CoreEntityMention> = sentence.entityMentions()
+        println("Example: entity mentions")
+        println(entityMentions)
+        println()
+
+        // coreference between entity mentions
+        val originalEntityMention: CoreEntityMention = document.sentences().get(3).entityMentions().get(1)
+        println("Example: original entity mention")
+        System.out.println(originalEntityMention)
+        println("Example: canonical entity mention")
+        System.out.println(originalEntityMention.canonicalEntityMention().get())
+        println()
+
+        // get document wide coref info
+        val corefChains: Map<Int, CorefChain> = document.corefChains()
+        println("Example: coref chains for document")
+        println(corefChains)
+        println()
+
+        // get quotes in document
+        val quotes: List<CoreQuote> = document.quotes()
+        val quote: CoreQuote = quotes[0]
+        println("Example: quote")
+        System.out.println(quote)
+        println()
+
+        // original speaker of quote
+        // note that quote.speaker() returns an Optional
+        println("Example: original speaker of quote")
+        System.out.println(quote.speaker().get())
+        println()
+
+        // canonical speaker of quote
+        println("Example: canonical speaker of quote")
+        System.out.println(quote.canonicalSpeaker().get())
+        println()*/
+    }
+}
